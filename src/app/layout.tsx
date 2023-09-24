c984576dceba19694f741170116e79416a1abd92
Henrique Marques <henrique_marques.dev@outlook.com>
2023-09-24 00:01:06 -0300

    fix: matadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import type { Metadata } from 'next';
 import { ThemeProvider } from 'styled-components';
 
--- src/contexts/NavbarContext.tsx
+++ src/contexts/NavbarContext.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { createContext, useState, useContext } from 'react';
 
 interface NavbarContextData {
