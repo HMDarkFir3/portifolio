8a8af244ae822771595e74ca7f065f5a015b2cb2
Henrique Marques <henrique_marques.dev@outlook.com>
2023-09-23 23:09:53 -0300

    fix: error dom

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,5 @@
+import { StyleSheetManager } from 'styled-components';
+
 import { useNavbar } from '@/contexts/NavbarContext';
 
 import { Navbar } from '@/components/Navbar';
@@ -15,28 +17,30 @@ export function Header(props: Props) {
   const { isOpenMenu, onToggleMenu } = useNavbar();
 
   return (
-    <Container isScrolled={isScrolled}>
-      <div className="wrapper">
-        <Logo>
-          <span className="name">
-            <a href="/">HM</a>
-          </span>
-        </Logo>
+    <StyleSheetManager shouldForwardProp={(prop) => prop !== 'isScrolled'}>
+      <Container isScrolled={isScrolled}>
+        <div className="wrapper">
+          <Logo>
+            <span className="name">
+              <a href="/">HM</a>
+            </span>
+          </Logo>
 
-        <ButtonMenu className="menuIcon" onClick={onToggleMenu}>
-          <div className={isOpenMenu ? 'bar barActive' : 'bar'} />
-          <div className={isOpenMenu ? 'bar barActive' : 'bar'} />
-          <div className={isOpenMenu ? 'bar barActive' : 'bar'} />
-        </ButtonMenu>
+          <ButtonMenu className="menuIcon" onClick={onToggleMenu}>
+            <div className={isOpenMenu ? 'bar barActive' : 'bar'} />
+            <div className={isOpenMenu ? 'bar barActive' : 'bar'} />
+            <div className={isOpenMenu ? 'bar barActive' : 'bar'} />
+          </ButtonMenu>
 
-        <Navbar isVisible={false} />
-      </div>
+          <Navbar isVisible={false} />
+        </div>
 
-      <div className={isOpenMenu ? 'menu menuActive' : 'menu'}>
-        <Navbar />
-      </div>
+        <div className={isOpenMenu ? 'menu menuActive' : 'menu'}>
+          <Navbar />
+        </div>
 
-      <Overlay />
-    </Container>
+        <Overlay />
+      </Container>
+    </StyleSheetManager>
   );
 }
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,4 +1,5 @@
 import { Link as ScrollLink } from 'react-scroll';
+import { StyleSheetManager } from 'styled-components';
 
 import { useNavbar } from '@/contexts/NavbarContext';
 
@@ -18,31 +19,33 @@ export function Navbar(props: Props) {
   const { onToggleMenu } = useNavbar();
 
   return (
-    <Container isVisible={isVisible}>
-      <ul className="list">
-        {links.map((link) => (
-          <ScrollLink
-            key={link.id}
-            to={link.slug}
-            smooth={true}
-            offset={-88}
-            duration={500}
-          >
-            <li className="item" onClick={onToggleMenu}>
-              <div className="listItemWrapper">
-                <span className={`${robotoMono.className} index`}>
-                  0{link.id}.
-                </span>
-                <span className={`${robotoMono.className} label`}>
-                  {link.label}
-                </span>
-              </div>
+    <StyleSheetManager shouldForwardProp={(prop) => prop !== 'isVisible'}>
+      <Container isVisible={isVisible}>
+        <ul className="list">
+          {links.map((link) => (
+            <ScrollLink
+              key={link.id}
+              to={link.slug}
+              smooth={true}
+              offset={-88}
+              duration={500}
+            >
+              <li className="item" onClick={onToggleMenu}>
+                <div className="listItemWrapper">
+                  <span className={`${robotoMono.className} index`}>
+                    0{link.id}.
+                  </span>
+                  <span className={`${robotoMono.className} label`}>
+                    {link.label}
+                  </span>
+                </div>
 
-              <div className="bar" />
-            </li>
-          </ScrollLink>
-        ))}
-      </ul>
-    </Container>
+                <div className="bar" />
+              </li>
+            </ScrollLink>
+          ))}
+        </ul>
+      </Container>
+    </StyleSheetManager>
   );
 }
