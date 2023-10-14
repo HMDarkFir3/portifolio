af0e25e0ec01594566c2b2239b289f929b9c70b5
Henrique Marques <henrique_marques.dev@outlook.com>
2023-10-13 23:14:06 -0300

    feat: experience section

---
+++ src/utils/experiences.ts
@@ -0,0 +1,34 @@
+export const experiences = [
+  {
+    id: '1',
+    title: 'Implementation Analyst',
+    company: 'CAF',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia provident voluptas impedit, error consequuntur similique nihil quaerat architecto dolore fuga accusamus laudantium repudiandae quisquam eius vitae, accusantium minus, quis aspernatur.',
+    period: 'AGO 2023 - PRESENT',
+  },
+  {
+    id: '2',
+    title: 'Front-End Developer',
+    company: 'Grão de Gente',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia provident voluptas impedit, error consequuntur similique nihil quaerat architecto dolore fuga accusamus laudantium repudiandae quisquam eius vitae, accusantium minus, quis aspernatur.',
+    period: 'OUT 2022 - FEV 2023',
+  },
+  {
+    id: '3',
+    title: 'Front-End Developer',
+    company: 'GBM Tech & Control',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia provident voluptas impedit, error consequuntur similique nihil quaerat architecto dolore fuga accusamus laudantium repudiandae quisquam eius vitae, accusantium minus, quis aspernatur.',
+    period: 'MAI 2022 - OUT 2022',
+  },
+  {
+    id: '4',
+    title: 'Software Development Intern',
+    company: 'GBM Tech & Control',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia provident voluptas impedit, error consequuntur similique nihil quaerat architecto dolore fuga accusamus laudantium repudiandae quisquam eius vitae, accusantium minus, quis aspernatur.',
+    period: 'NOV 2021 - MAI 2022',
+  },
+];
